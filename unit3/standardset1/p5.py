de195f356e3faa5b490a5e7ce9eed536a6bb254d
Steven Uy <81347262+Steven-UY@users.noreply.github.com>
2025-06-18 13:26:43 -0400

    yay

--- unit3/standardset1/p5.py
+++ unit3/standardset1/p5.py
@@ -5,7 +5,29 @@
 '''
 
 def merge_schedules(schedule1, schedule2):
+
+    pointer_1 = 0
+    pointer_2 = 0
+
+    res = ""
+
+    while pointer_1 < len(schedule1) and pointer_2 < len(schedule2):
+
+        res += schedule1[pointer_1]
+        pointer_1 += 1
+
+        res += schedule2[pointer_2]
+        pointer_2 += 1
     
+    if pointer_1 < len(schedule1):
+        res += schedule1[pointer_1:]
+    elif pointer_2 < len(schedule2):
+        res += schedule2[pointer_2:]
+    
+    return res
+
+
+
 
 print(merge_schedules("abc", "pqr")) 
 print(merge_schedules("ab", "pqrs")) 
